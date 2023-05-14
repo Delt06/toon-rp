0f20ea9bbacbcf1890834f4d4392921006826e3b
Vladislav Kantaev <vvkantaev@yandex.ru>
2023-05-14 19:01:01 +0200

    Implement getting value from a property for ShowIf

--- Packages/com.deltation.toon-rp/Editor/ToonRpShowIfPropertyDrawer.cs
+++ Packages/com.deltation.toon-rp/Editor/ToonRpShowIfPropertyDrawer.cs
@@ -1,4 +1,8 @@
-﻿using DELTation.ToonRP.Attributes;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using DELTation.ToonRP.Attributes;
 using JetBrains.Annotations;
 using UnityEditor;
 using UnityEngine.UIElements;
@@ -8,15 +12,73 @@ namespace DELTation.ToonRP.Editor
     [CustomPropertyDrawer(typeof(ToonRpShowIfAttribute))]
     public class ToonRpShowIfPropertyDrawer : ToonRpEditorDecorator
     {
+        private const string PathSeparator = ".";
+
+        private const BindingFlags MemberBindingFlags = BindingFlags.Instance | BindingFlags.Static |
+                                                        BindingFlags.Public | BindingFlags.NonPublic;
         [CanBeNull]
         private string _conditionPath;
+        [CanBeNull]
+        private Func<bool?> _getPropertyValue;
+
+        private static IEnumerable<string> BuildPropertyPathPieces(string basePath, int lastSkips,
+            params string[] appends)
+        {
+            string[] propertyPathPieces = basePath.Split(PathSeparator);
+            return propertyPathPieces.SkipLast(lastSkips).Concat(appends);
+        }
+
+        private static string BuildPropertyPath(string basePath, int lastSkips, params string[] appends) =>
+            string.Join(PathSeparator, BuildPropertyPathPieces(basePath, lastSkips, appends));
+
+        private static object GetFieldValueRecursive(object currentObject, IEnumerable<string> pathPieces)
+        {
+            foreach (string fieldName in pathPieces)
+            {
+                Type type = currentObject.GetType();
+                FieldInfo field = type.GetField(fieldName, MemberBindingFlags);
+                if (field == null)
+                {
+                    throw new ArgumentException($"Could not find field {fieldName} in {type}.", nameof(pathPieces));
+                }
+
+                currentObject = field.GetValue(currentObject);
+            }
+
+            return currentObject;
+        }
 
         protected override VisualElement AddProperty(VisualElement root, SerializedProperty property)
         {
             var showIfAttribute = (ToonRpShowIfAttribute) attribute;
-            string[] pathPieces = property.propertyPath.Split(".");
-            pathPieces[^1] = showIfAttribute.FieldName;
-            _conditionPath = string.Join(".", pathPieces);
+            string basePropertyPath = property.propertyPath;
+
+            {
+                _conditionPath = BuildPropertyPath(basePropertyPath, 1, showIfAttribute.FieldName);
+            }
+
+            {
+                if (showIfAttribute.FieldName != null)
+                {
+                    IEnumerable<string> propertyPathPieces = BuildPropertyPathPieces(basePropertyPath, 1);
+
+                    _getPropertyValue = () =>
+                    {
+                        object baseObject =
+                            GetFieldValueRecursive(property.serializedObject.targetObject, propertyPathPieces);
+                        Type type = baseObject.GetType();
+                        PropertyInfo propertyInfo = type.GetProperty(showIfAttribute.FieldName, MemberBindingFlags);
+                        return propertyInfo?.PropertyType == typeof(bool)
+                            ? (bool) propertyInfo.GetValue(baseObject)
+                            : null;
+                    };
+                    if (_getPropertyValue() == null)
+                    {
+                        _getPropertyValue = null;
+                    }
+                }
+            }
+
 
             var imguiContainer = new IMGUIContainer();
 
@@ -33,7 +95,20 @@ namespace DELTation.ToonRP.Editor
                 SerializedProperty conditionProperty = property.serializedObject.FindProperty(_conditionPath);
                 if (conditionProperty == null)
                 {
-                    EditorGUILayout.HelpBox($"Could not find a property at {_conditionPath}", MessageType.Error);
+                    if (_getPropertyValue != null)
+                    {
+                        bool? propertyValue = _getPropertyValue();
+                        if (propertyValue.HasValue)
+                        {
+                            visualElement.SetVisible(propertyValue.Value);
+                        }
+
+                        return;
+                    }
+
+                    EditorGUILayout.HelpBox($"Could not find the property {showIfAttribute.FieldName}",
+                        MessageType.Error
+                    );
                     return;
                 }
 
--- Packages/com.deltation.toon-rp/Runtime/PostProcessing/BuiltIn/ToonFxaaSettings.cs
+++ Packages/com.deltation.toon-rp/Runtime/PostProcessing/BuiltIn/ToonFxaaSettings.cs
@@ -10,14 +10,16 @@ namespace DELTation.ToonRP.PostProcessing.BuiltIn
         public bool Enabled;
         [ToonRpShowIf(nameof(Enabled))]
         public bool HighQuality;
-        [ToonRpShowIf(nameof(Enabled))]
+        [ToonRpShowIf(nameof(IsEnabledAndHighQuality))]
         [Min(0.0f)]
         public float FixedContrastThresholdId;
-        [ToonRpShowIf(nameof(Enabled))]
+        [ToonRpShowIf(nameof(IsEnabledAndHighQuality))]
         [Min(0.0f)]
         public float RelativeContrastThreshold;
-        [ToonRpShowIf(nameof(Enabled))]
+        [ToonRpShowIf(nameof(IsEnabledAndHighQuality))]
         [Range(0.0f, 1.0f)]
         public float SubpixelBlendingFactor;
+
+        private bool IsEnabledAndHighQuality => Enabled && HighQuality;
     }
 }

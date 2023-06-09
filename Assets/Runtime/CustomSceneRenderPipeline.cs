767316fac6f9caae1dce83ed059fa3f17f380f89
Vladislav Kantaev <vvkantaev@yandex.ru>
2023-06-10 00:51:16 +0200

    Add custom scene render pipeline

---
+++ Assets/Runtime/CustomSceneRenderPipeline.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Runtime
+{
+    [ExecuteAlways]
+    public sealed class CustomSceneRenderPipeline : MonoBehaviour
+    {
+        [SerializeField] private RenderPipelineAsset _pipelineAsset;
+
+        private void OnEnable()
+        {
+            SetPipeline();
+        }
+
+        private void OnValidate()
+        {
+            SetPipeline();
+        }
+
+        private void SetPipeline()
+        {
+            if (_pipelineAsset != null)
+            {
+                GraphicsSettings.renderPipelineAsset = _pipelineAsset;
+            }
+        }
+    }
+}

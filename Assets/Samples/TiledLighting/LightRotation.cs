57e839df6c8cff6d7859df22660260aba654af2f
Vladislav Kantaev <vvkantaev@yandex.ru>
2023-10-09 21:06:35 +0200

    Finalize the tiled lighting scene

---
+++ Assets/Samples/TiledLighting/LightRotation.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Samples.TiledLighting
+{
+    public class LightRotation : MonoBehaviour
+    {
+        [SerializeField] private Vector2 _speedRange;
+
+        private (Light light, float speed)[] _lights;
+
+
+        private void Awake()
+        {
+            _lights = FindObjectsOfType<Light>()
+                .Where(l => l.type == LightType.Point)
+                .Select(l =>
+                    {
+                        float speed = Random.Range(_speedRange.x, _speedRange.y);
+                        if (Random.value > 0.5f)
+                        {
+                            speed *= -1;
+                        }
+
+                        return (l, speed);
+                    }
+                )
+                .ToArray();
+        }
+
+        private void Update()
+        {
+            Vector3 origin = transform.position;
+
+            foreach ((Light l, float speed) in _lights)
+            {
+                Transform lightTransform = l.transform;
+                Vector3 position = lightTransform.position;
+                Vector3 offset = position - origin;
+                offset = Quaternion.Euler(0, speed * Time.deltaTime, 0) * offset;
+                lightTransform.position = origin + offset;
+            }
+        }
+    }
+}

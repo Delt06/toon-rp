837f2e6f3a2f284d37fbef4ad15b0309965c56a9
Vladislav Kantaev <vvkantaev@yandex.ru>
2023-10-14 18:31:39 +0200

    Allow for sharpen amount up to 5

--- Packages/com.deltation.toon-rp/Runtime/PostProcessing/BuiltIn/ToonSharpenSettings.cs
+++ Packages/com.deltation.toon-rp/Runtime/PostProcessing/BuiltIn/ToonSharpenSettings.cs
@@ -6,7 +6,7 @@ namespace DELTation.ToonRP.PostProcessing.BuiltIn
     [Serializable]
     public struct ToonSharpenSettings
     {
-        [Range(0.0f, 1.0f)]
+        [Range(-0.0f, 10.0f)]
         public float Amount;
     }
 }

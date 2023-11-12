b8ab049185714d3cae4b2c3d8aee4bc52e19a381
Vladislav Kantaev <vvkantaev@yandex.ru>
2023-11-12 12:49:58 +0100

    Fix missing stencil in ToonFormatUtils.DefaultDepthStencilFormat

--- Packages/com.deltation.toon-rp/Runtime/ToonFormatUtils.cs
+++ Packages/com.deltation.toon-rp/Runtime/ToonFormatUtils.cs
@@ -5,7 +5,7 @@ namespace DELTation.ToonRP
     public static class ToonFormatUtils
     {
         public const GraphicsFormat DefaultDepthFormat = GraphicsFormat.D16_UNorm;
-        public const GraphicsFormat DefaultDepthStencilFormat = GraphicsFormat.D16_UNorm;
+        public const GraphicsFormat DefaultDepthStencilFormat = GraphicsFormat.D16_UNorm_S8_UInt;
 
         public static GraphicsFormat GetDefaultDepthFormat(bool stencil) =>
             stencil ? DefaultDepthStencilFormat : DefaultDepthFormat;

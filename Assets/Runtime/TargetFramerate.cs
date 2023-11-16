9e457a56946358c2bda3ade179b467003bdd01c9
Vladislav Kantaev <vvkantaev@yandex.ru>
2023-11-16 21:33:48 +0100

    Extend main demo (more quality settings, remove the FPS limit)

---
+++ Assets/Runtime/TargetFramerate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Runtime
+{
+    public class TargetFramerate : MonoBehaviour
+    {
+        [SerializeField] private int _targetFramerate = 60;
+
+        private void Awake()
+        {
+            Application.targetFrameRate = _targetFramerate;
+        }
+    }
+}

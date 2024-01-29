4dcdd0681572c80061ffe743fb4175735b64380a
Vladislav Kantaev <vvkantaev@yandex.ru>
2024-01-29 23:27:57 +0100

    Add smear effect

---
+++ Assets/Samples/StylizedEffects/Effects/Smear/SmearEffect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+// https://github.com/cjacobwade/HelpfulScripts/blob/master/SmearEffect/SmearEffect.cs
+namespace Samples.StylizedEffects.Effects.Smear
+{
+    public class SmearEffect : MonoBehaviour
+    {
+        private static readonly int MotionId = Shader.PropertyToID("_Motion");
+
+        [SerializeField] private Renderer _renderer;
+        [SerializeField] [Min(0)] private int _frameLag;
+
+        private readonly Queue<Vector3> _recentPositions = new();
+
+        private Material _material;
+
+        private void Start()
+        {
+            _material = _renderer.material;
+        }
+
+        private void LateUpdate()
+        {
+            Vector3 currentPosition = transform.position;
+
+            if (_recentPositions.Count > _frameLag)
+            {
+                _material.SetVector(MotionId, currentPosition - _recentPositions.Dequeue());
+            }
+
+            _recentPositions.Enqueue(currentPosition);
+        }
+    }
+}
---
+++ Assets/Samples/StylizedEffects/Effects/Smear/SmearInclude.hlsl
@@ -0,0 +1,27 @@
+﻿#ifndef SMEAR_HLSL
+#define SMEAR_HLSL
+
+// https://github.com/cjacobwade/HelpfulScripts/blob/master/SmearEffect/Smear.shader
+
+float Hash(const float n)
+{
+    return frac(sin(n) * 43758.5453);
+}
+
+void SmearPositionNoise_float(const float3 position, out float noise)
+{
+    // The noise function returns a value in the range -1.0f -> 1.0f
+
+    float3 p = floor(position);
+    float3 f = frac(position);
+
+    f = f * f * (3.0 - 2.0 * f);
+    const float n = p.x + p.y * 57.0 + 113.0 * p.z;
+
+    noise = lerp(lerp(lerp(Hash(n + 0.0), Hash(n + 1.0), f.x),
+                      lerp(Hash(n + 57.0), Hash(n + 58.0), f.x), f.y),
+                 lerp(lerp(Hash(n + 113.0), Hash(n + 114.0), f.x),
+                      lerp(Hash(n + 170.0), Hash(n + 171.0), f.x), f.y), f.z);
+}
+
+#endif // SMEAR_HLSL

6335d4129dd0f9481934613aa0f100a693b52357
Vladislav Kantaev <vvkantaev3@gmail.com>
2024-02-13 23:20:52 +0100

    Add screen shake to stylized effects

---
+++ Assets/Samples/StylizedEffects/ScreenShakeInput.cs
@@ -0,0 +1,24 @@
+﻿using DELTation.ToonRP;
+using DELTation.ToonRP.PostProcessing.BuiltIn;
+using UnityEngine;
+
+namespace Samples.StylizedEffects
+{
+    public class ScreenShakeInput : MonoBehaviour
+    {
+        public float Magnitude = 0.1f;
+        public float Duration = 0.5f;
+        public ToonAdditionalCameraData CameraData;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ToonScreenSpaceShakePersistentData persistentData =
+                    CameraData.GetPersistentData<ToonScreenSpaceShakePersistentData>();
+                var shake = new ToonScreenSpaceShakePersistentData.Shake(Magnitude, Duration);
+                persistentData.AddShake(shake);
+            }
+        }
+    }
+}

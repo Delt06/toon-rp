059a897b5603e5df7aaabf3b2dc5f06183e4ec7e
Vladislav Kantaev <vvkantaev3@gmail.com>
2024-04-21 16:57:03 +0200

    Add motion blur demo

---
+++ Assets/Samples/MotionBlur/TimeScaleController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Samples.MotionBlur
+{
+    public class TimeScaleController : MonoBehaviour
+    {
+        [Min(0.0f)]
+        public float TimeScale = 1.0f;
+
+        private void Update()
+        {
+            Time.timeScale = TimeScale;
+        }
+    }
+}

4c07e8a5e031712dd3613249904ac1063b625943
Vladislav Kantaev <vvkantaev3@gmail.com>
2024-07-14 10:19:46 +0200

    Increase min depth bits to 16->24

--- Packages/com.deltation.toon-rp/Runtime/ToonFormatUtils.cs
+++ Packages/com.deltation.toon-rp/Runtime/ToonFormatUtils.cs
@@ -4,7 +4,7 @@ namespace DELTation.ToonRP
 {
     public static class ToonFormatUtils
     {
-        private const int DepthBits = 16;
+        private const int DepthBits = 24;
         public static readonly GraphicsFormat DefaultDepthFormat =
             GraphicsFormatUtility.GetDepthStencilFormat(DepthBits, 0);
         public static readonly GraphicsFormat DefaultDepthStencilFormat =

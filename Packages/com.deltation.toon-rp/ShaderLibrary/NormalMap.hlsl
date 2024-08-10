68e103f11ed5e2bfde1a927f7e9c86bf28b6cfad
Vladislav Kantaev <vvkantaev3@gmail.com>
2024-08-10 13:51:42 +0200

    Add SampleNormal with a scale parameter

--- Packages/com.deltation.toon-rp/ShaderLibrary/NormalMap.hlsl
+++ Packages/com.deltation.toon-rp/ShaderLibrary/NormalMap.hlsl
@@ -9,6 +9,12 @@ half3 SampleNormal(const float2 uv, TEXTURE2D_PARAM(bumpMap, sampler_bumpMap))
     return UnpackNormal(pn);
 }
 
+half3 SampleNormal(const float2 uv, TEXTURE2D_PARAM(bumpMap, sampler_bumpMap), const float scale)
+{
+    const float4 pn = SAMPLE_TEXTURE2D(bumpMap, sampler_bumpMap, uv);
+    return UnpackNormalScale(pn, scale);
+}
+
 half3 TransformTangentToWorld(const half3 normalTs, const half3x3 tbn)
 {
     return mul(normalTs, tbn);
